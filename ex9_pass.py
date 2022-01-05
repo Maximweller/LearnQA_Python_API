67f4956939c68857e3f4502cfebd35f0b2263084
Guseyn Salimov <maximweller@protonmail.com>
2022-01-05 16:12:31 +0300

    minor fixes
    gitignore, requirements, virtual environment, minor fixes in ex9_pass

--- ex9_pass.py
+++ ex9_pass.py
@@ -6,7 +6,8 @@ login = 'super_admin'
 # Добываем список паролей и делаем этот список уникальным
 response = requests.get("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
 tree = html.fromstring(response.text)
-locator = '//*[contains(text(),"Top 25 most common passwords by year according to SplashData")]//..//td[@align="left"]/text()'
+locator = '//*[contains(text(),' \
+          '"Top 25 most common passwords by year according to SplashData")]//..//td[@align="left"]/text()'
 passwords = tree.xpath(locator)
 uni_passwords = list(set(passwords))
 true_pass = ''
@@ -18,14 +19,14 @@ check_url = 'https://playground.learnqa.ru/ajax/api/check_auth_cookie'
 # Во втором запросе получаем ответ, если ответ положительный, то прерываем цикл
 for password in uni_passwords:
     password = str(password).strip()
-    #print(password)
+    # print(password)
     payload = {"login": login, "password": password}
     response1 = requests.post(url=cookie_url, data=payload)
-    #print(dict(response1.cookies))
+    # print(dict(response1.cookies))
     cookie_value = response1.cookies.get('auth_cookie')
     cookies = {'auth_cookie': cookie_value}
     response2 = requests.post(url=check_url, cookies=cookies)
-    #print(response2.text)
+    # print(response2.text)
     if response2.text == 'You are authorized':
         true_pass = password
         break
